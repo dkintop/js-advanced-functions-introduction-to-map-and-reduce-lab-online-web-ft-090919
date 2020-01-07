3a601d15240b6e9cedbcb8e13ae383bb6431329a
Doug Kintop <dkintop@hotmail.com>
2020-01-07 12:44:18 -0500

    Done.

--- index.js
+++ index.js
@@ -25,3 +25,25 @@ function mapToSquare(array) {
   });
   return output;
 }
+
+function reduceToTotal(src, startingPoint = 0) {
+  let total = startingPoint;
+  for (let i = 0; i < src.length; i++) {
+    total = total + src[i];
+  }
+  return total;
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++) {
+    if (!src[i]) return false;
+  }
+  return true;
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++) {
+    if (src[i]) return true;
+  }
+  return false;
+}
